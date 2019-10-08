f8f077378cb31e667566a002b47a2cd398428eb6
zsuih <zsuih@NikkiZhouGerblick>
2019-10-08 10:02:26 -0700

    First Change.

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -8,23 +8,26 @@ public class AddingMachine {
 		total = 0;  // not needed - included for clarity
 	}
 	
-	public int getTotal () {  // 
-		return 0;
+	public int getTotal () {  // return total value from calculation
+		return total;
 	}
 	
 	public void add (int value) {  // Addition Method
+		total += value ;
 		
 	}
 	
 	public void subtract (int value) {  // Subtraction Method
+		total -= value;					
 		
 	}
 		
-	public String toString () {   
+	public String toString () {       // tracking the history of operator uses
 		return "";
 	}
 
-	public void clear() {               // Free memory
+	public void clear() {               // clean up and free the heap after program done.
+
 	
 	}
 }
