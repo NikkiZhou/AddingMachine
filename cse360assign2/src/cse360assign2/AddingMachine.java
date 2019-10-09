c83c9598d09f625c21c72649aac1d052e4ba2796
zsuih <zsuih@NikkiZhouGerblick>
2019-10-09 11:52:22 -0700

    Update all documentations.

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -8,46 +8,78 @@ package cse360assign2;
  */
 
 public class AddingMachine {
-	/**
-	 * This is a program by using operator + and/or - as a calculator.
-	 * 
-	 */
+	
+/**
+ * This is a program by using operator + and/or - as a calculator.
+ * 
+ */
+	
 
-	private int total;
+	private int total;	
+	
 	
 	private StringBuffer history;  // additional variable
 	
+	/**
+	 * Use StringBuffer to satisfy no limit on the size of the String returned.
+	 */
+	
+	
 	public AddingMachine () {
 		
-		total = 0;  // not needed - included for clarity
-		history = new StringBuffer("0");  // Set the initial value of the history 0.
+		total = 0;                         // not needed - included for clarity
+		
+		history = new StringBuffer("0");   // Set the initial value of the history 0.
+		
 	}
 	
-	public int getTotal () {  // return total value from calculation
+	
+	public int getTotal () {               // return total value from calculation
 		
 		return total;
 	}
 	
-	public void add (int value) {  // Create addition function
+	
+	public void add (int value) {     
+		
+	/**
+	 * Create addition function, then merge operator + with parameter.
+	 * @param value add it to the total variable.
+	 * 
+	 */
 		
 		total += value ;
-		history.append(" + " + value);  // merge operator + with value
+		history.append(" + " + value);     
 		
 	}
 	
-	public void subtract (int value) {  // Create subtraction function
+	public void subtract (int value) {
+		
+	/**
+	 * Create subtraction function, then merge operator - with parameter.
+	 * @param value subtract it from the total variable.
+	 * 
+	 */
 		
 		total -= value;	
-		history.append(" - " + value);  // merge operator - with value
+		history.append(" - " + value);   
 		
 	}
+	
 		
-	public String toString () {       // A history of the transactions must be kept to be returned by the toString method.
-		return history.toString();               // return history of the transactions: 0 + 4 – 2 + 5
+	public String toString () {
+	
+	/**
+	 * A history of the transactions must be kept to be returned by the toString method.   
+	 */
+		return history.toString();         // return history of the transactions: 0 + 4 – 2 + 5
 	}
 
-	public void clear() {               // clear total and the history string
-		
+	public void clear() {  
+
+	/**
+	 * Clear the total variable and the history string.
+	 */
 		total = 0;                     // set total to zero.
 		history.setLength(1);         // Set the history string back to zero for the next calculation.
 		
@@ -66,6 +98,7 @@ public class AddingMachine {
 		myCalculator.subtract(2);
 		
 		myCalculator.add(5);
+		
 				
 		/**
 		 * Using standard output stream for giving the output.
@@ -74,10 +107,12 @@ public class AddingMachine {
 		
 		System.out.println("History:" + myCalculator.toString() + '\n');
 		
+		
 		/**
 		 * Call clear() method to clear total.
 		 */
-		myCalculator.clear();			// Clearing the Set using clear() method 	
+		myCalculator.clear();
+		
 		
 		System.out.println("Total Deleted: " + myCalculator.getTotal() + '\n'); // Displaying the total
 		
